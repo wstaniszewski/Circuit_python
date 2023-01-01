502dd262367c4c70389b30eb253cf72003be5d6a
wstan <w.staniszewski@gmail.com>
2023-01-01 16:01:16 +0100

    initial working

---
+++ secrets.py
@@ -0,0 +1,8 @@
+secrets = {
+    'ssid' : 'wojtek24',
+    'password' : 'staniszewski123456',
+    'aio_username' : 'wojtek',
+    'aio_key' : '69AGjfsuWm8aMviMtTfdMPCpjz68mj',
+    'broker' : '192.168.0.10',
+    'port' : 1883
+    }
